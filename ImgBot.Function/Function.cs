be2ec631480a65b12c4da755c8f00588fca60fb2
Dan Butvinik <dabutvin@microsoft.com>
2017-08-24 16:16:49 +0000

    commit and push a branch with optimizations

--- ImgBot.Function/Function.cs
+++ ImgBot.Function/Function.cs
@@ -26,13 +26,33 @@ namespace ImgBot.Function
             var imgPatterns = new[] { "*.png", "*.jpg", "*.jpeg", "*.gif", };
             var images = imgPatterns.AsParallel().SelectMany(pattern => Directory.EnumerateFiles(localPath, pattern, SearchOption.AllDirectories)).ToArray();
 
+            // check out a branch
+            var repo = new Repository(localPath);
+            repo.CreateBranch("imgbot");
+            var branch = Commands.Checkout(repo, "imgbot");
 
             // optimize images
             ImageOptimizer imageOptimizer = new ImageOptimizer();
-            foreach (var inputPath in images)
+            foreach (var image in images)
             {
-                try { imageOptimizer.LosslessCompress(inputPath); } catch { }
+                try { imageOptimizer.LosslessCompress(image); } catch { }
+                Commands.Stage(repo, image);
             }
+
+            // commit
+            var signature = new Signature("imgbot", "imgbot@gmail.com", DateTimeOffset.Now);
+            repo.Commit("[ImgBot] optimizes images", signature, signature);
+
+            // push to GitHub
+            var remote = repo.Network.Remotes["origin"];
+
+            var options = new PushOptions
+            {
+                CredentialsProvider = (_url, _user, _cred) =>
+                    new UsernamePasswordCredentials { Username = "dabutvin", Password = "XXXXXXXXXXXX" }
+            };
+
+            repo.Network.Push(remote, @"refs/heads/imgbot", options);
         }
     }
 
