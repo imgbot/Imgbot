d8b59b486e5113b4b539b82416b13d8230c32f74
Dan Butvinik <dabutvin@microsoft.com>
2017-08-24 06:21:59 +0000

    initial commit

---
+++ ImgBot.Function/Function.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace ImgBot.Function
+{
+    public static class Function
+    {
+        [FunctionName("QueueTrigger")]        
+        public static void Run([QueueTrigger("analyze", Connection = "")]AnalyzeMessage analyzeMessage, TraceWriter log, ExecutionContext context)
+        {
+            log.Info($"C# Queue trigger function processed: {context.FunctionDirectory}");
+
+            var localPath = Path.Combine(context.FunctionDirectory, analyzeMessage.RepoName +  new Random().Next(100,99999).ToString());
+
+
+            // clone
+            Repository.Clone(analyzeMessage.CloneUrl, localPath);
+
+
+            // extract images
+            var imgPatterns = new[] { "*.png", "*.jpg", "*.jpeg", "*.gif", };
+            var images = imgPatterns.AsParallel().SelectMany(pattern => Directory.EnumerateFiles(localPath, pattern, SearchOption.AllDirectories)).ToArray();
+
+
+            // optimize images
+
+            foreach (var inputPath in images)
+            {
+                using (var input = File.OpenRead(inputPath))
+                {
+                    using (var output = File.OpenWrite("C:\\tmp\\img1.png"))
+                    {
+                        var image = new Image(input);
+                    }
+                }
+            }
+
+        }
+    }
+
+    public class AnalyzeMessage
+    {
+        public string CloneUrl { get; set; }
+        public string RepoName { get; set; }
+    }
+}
---
+++ ImgBot.Web/Controllers/HookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImgBot.Web.Controllers
+{
+    public class HookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(new { data = true });
+        }
+    }
+}
