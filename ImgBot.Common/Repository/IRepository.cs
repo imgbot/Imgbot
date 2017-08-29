4381204575f1f3c8bdd3576ca890dd8034f57542
Dan Butvinik <dabutvin@microsoft.com>
2017-08-29 07:39:47 +0000

    handles uninstallations (fixes #5)

---
+++ ImgBot.Common/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ImgBot.Common.Repository
+{
+    public interface IRepository
+    {
+        Task InsertOrMergeAsync<T>(T entity) where T : TableEntity, new();
+        Task<T> RetrieveAsync<T>(string partitionKey, string rowKey) where T : TableEntity, new();
+        Task DeleteAsync<T>(string partitionKey, string rowKey) where T : TableEntity, new();
+        Task<T[]> RetrievePartitionAsync<T>(string partitionKey) where T : TableEntity, new();
+    }
+}
