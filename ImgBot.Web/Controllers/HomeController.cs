af7e2aac7df91b0953ec3d003844352aa63caf6b
Dan Butvinik <dabutvin@microsoft.com>
2017-08-29 08:49:32 +0000

    adds privacy policy

--- ImgBot.Web/Controllers/HomeController.cs
+++ ImgBot.Web/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ImgBot.Web.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
