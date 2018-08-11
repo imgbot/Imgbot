3ebe15edc9036c4bea2a8a60f9d8bfc4d730bce8
dabutvin <butvinik@outlook.com>
2018-08-11 09:15:15 -0700

    adds failsafe for too strong compression (fixes #141)

--- CompressImagesFunction/CompressImages.cs
+++ CompressImagesFunction/CompressImages.cs
@@ -76,6 +76,11 @@ namespace CompressImagesFunction
             if (optimizedImages.Length == 0)
                 return false;
 
+            // Fail safe for overly optimized images
+            // https://github.com/dabutvin/ImgBot/issues/141
+            if (optimizedImages.Any(x => x.PercentSaved > 80))
+                return false;
+
             // create commit message based on optimizations
             var commitMessage = CommitMessage.Create(optimizedImages);
 
