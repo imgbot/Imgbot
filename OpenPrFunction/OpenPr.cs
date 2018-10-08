8351aecd39d4fd5220e966fbebd8714594ad2f4a
dabutvin <butvinik@outlook.com>
2018-10-08 08:54:11 -0700

    fixes indentation

--- OpenPrFunction/OpenPr.cs
+++ OpenPrFunction/OpenPr.cs
@@ -10,55 +10,55 @@ using Microsoft.Extensions.Logging;
 
 namespace OpenPrFunction
 {
-  public static class OpenPr
-  {
-    [Singleton("{InstallationId}")] // https://github.com/Azure/azure-webjobs-sdk/wiki/Singleton#scenarios
-    [FunctionName("OpenPr")]
-    public static async Task Trigger(
-        [QueueTrigger("openprmessage")]OpenPrMessage openPrMessage,
-        [Table("installation", "{InstallationId}", "{RepoName}")] Installation installation,
-        ILogger logger,
-        ExecutionContext context)
+    public static class OpenPr
     {
-      var installationTokenProvider = new InstallationTokenProvider();
-      var pullRequest = new PullRequest();
-      await RunAsync(openPrMessage, installation, installationTokenProvider, pullRequest, logger, context);
+        [Singleton("{InstallationId}")] // https://github.com/Azure/azure-webjobs-sdk/wiki/Singleton#scenarios
+        [FunctionName("OpenPr")]
+        public static async Task Trigger(
+            [QueueTrigger("openprmessage")]OpenPrMessage openPrMessage,
+            [Table("installation", "{InstallationId}", "{RepoName}")] Installation installation,
+            ILogger logger,
+            ExecutionContext context)
+        {
+            var installationTokenProvider = new InstallationTokenProvider();
+            var pullRequest = new PullRequest();
+            await RunAsync(openPrMessage, installation, installationTokenProvider, pullRequest, logger, context);
+        }
+
+        public static async Task RunAsync(
+            OpenPrMessage openPrMessage,
+            Installation installation,
+            IInstallationTokenProvider installationTokenProvider,
+            IPullRequest pullRequest,
+            ILogger logger,
+            ExecutionContext context)
+        {
+            if (installation == null)
+            {
+                logger.LogError("No installation found for {InstallationId}", openPrMessage.InstallationId);
+                throw new Exception($"No installation found for InstallationId: {openPrMessage.InstallationId}");
+            }
+
+            var installationToken = await installationTokenProvider.GenerateAsync(
+                new InstallationTokenParameters
+                {
+                    AccessTokensUrl = string.Format(KnownGitHubs.AccessTokensUrlFormat, installation.InstallationId),
+                    AppId = KnownGitHubs.AppId,
+                },
+                File.OpenText(Path.Combine(context.FunctionDirectory, $"../{KnownGitHubs.AppPrivateKey}")));
+
+            logger.LogInformation("OpenPrFunction: Opening pull request for {Owner}/{RepoName}", installation.Owner, installation.RepoName);
+            var id = await pullRequest.OpenAsync(new PullRequestParameters
+            {
+                Password = installationToken.Token,
+                RepoName = installation.RepoName,
+                RepoOwner = installation.Owner,
+            });
+
+            if (id > 0)
+            {
+                logger.LogInformation("OpenPrFunction: Successfully opened pull request (#{PullRequestId}) for {Owner}/{RepoName}", id, installation.Owner, installation.RepoName);
+            }
+        }
     }
-
-    public static async Task RunAsync(
-        OpenPrMessage openPrMessage,
-        Installation installation,
-        IInstallationTokenProvider installationTokenProvider,
-        IPullRequest pullRequest,
-        ILogger logger,
-        ExecutionContext context)
-    {
-      if (installation == null)
-      {
-        logger.LogError("No installation found for {InstallationId}", openPrMessage.InstallationId);
-        throw new Exception($"No installation found for InstallationId: {openPrMessage.InstallationId}");
-      }
-
-      var installationToken = await installationTokenProvider.GenerateAsync(
-          new InstallationTokenParameters
-          {
-            AccessTokensUrl = string.Format(KnownGitHubs.AccessTokensUrlFormat, installation.InstallationId),
-            AppId = KnownGitHubs.AppId,
-          },
-          File.OpenText(Path.Combine(context.FunctionDirectory, $"../{KnownGitHubs.AppPrivateKey}")));
-
-      logger.LogInformation("OpenPrFunction: Opening pull request for {Owner}/{RepoName}", installation.Owner, installation.RepoName);
-      var id = await pullRequest.OpenAsync(new PullRequestParameters
-      {
-        Password = installationToken.Token,
-        RepoName = installation.RepoName,
-        RepoOwner = installation.Owner,
-      });
-
-      if (id > 0)
-      {
-        logger.LogInformation("OpenPrFunction: Successfully opened pull request (#{PullRequestId}) for {Owner}/{RepoName}", id, installation.Owner, installation.RepoName);
-      }
-    }
-  }
 }
