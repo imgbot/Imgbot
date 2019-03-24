77bf9c2c7e5ccad8d5b98fea26fbc73b4f308d6e
dabutvin <butvinik@outlook.com>
2019-03-24 10:12:27 -0700

    check if imgbot branch already exists before cloning

--- CompressImagesFunction/RepoChecks.cs
+++ CompressImagesFunction/RepoChecks.cs
@@ -26,5 +26,24 @@ namespace CompressImagesFunction
                 return true; // not found can be treated the same as archived
             }
         }
+
+        public async Task<bool> BranchExists(GitHubClientParameters parameters)
+        {
+            var inMemoryCredentialStore = new InMemoryCredentialStore(
+                new Credentials(KnownGitHubs.Username, parameters.Password));
+
+            var githubClient = new GitHubClient(
+                new ProductHeaderValue("ImgBot"), inMemoryCredentialStore);
+            try
+            {
+                var branch = await githubClient.Repository.Branch.Get(
+                    parameters.RepoOwner, parameters.RepoName, KnownGitHubs.BranchName);
+                return branch?.Commit != null;
+            }
+            catch (NotFoundException)
+            {
+                return false; // not found can be treated the same as does not exist
+            }
+        }
     }
 }
