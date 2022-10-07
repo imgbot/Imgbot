9e4f4b8c8583f6ccd5facaa06d82752067124cff
GrigoreMihai <grigoremihai56@gmail.com>
2022-10-07 19:38:07 +0300

    chore: started to add sync workflow between database and git data

--- MarketplaceSyncFunction/MarketplaceSync.cs
+++ MarketplaceSyncFunction/MarketplaceSync.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -22,7 +23,8 @@ namespace MarketplaceSyncFunction
         [Singleton]
         [FunctionName("MarketplaceSync")]
         public static async Task TimerTrigger(
-            [TimerTrigger("0 0 * * * *", RunOnStartup = true)]TimerInfo timerInfo,
+            [TimerTrigger("0 0 * * * *", RunOnStartup = true)]
+            TimerInfo timerInfo,
             ILogger logger,
             ExecutionContext context)
         {
@@ -45,10 +47,12 @@ namespace MarketplaceSyncFunction
                     AppId = KnownGitHubs.AppId,
                 },
                 KnownEnvironmentVariables.APP_PRIVATE_KEY);
-            var currentPlans = KnownGitHubs.Plans.Keys.Where(k => KnownGitHubs.Plans[k] == -1 || KnownGitHubs.Plans[k] >= KnownGitHubs.SmallestLimitPaidPlan);
+            var currentPlans = KnownGitHubs.Plans.Keys.Where(k =>
+                KnownGitHubs.Plans[k] == -1 || KnownGitHubs.Plans[k] >= KnownGitHubs.SmallestLimitPaidPlan);
 
             foreach (var planId in currentPlans)
             {
+                // TODO this will need to be updated to get more than 100 purchases per plan (loop with page) when we get more of the new plans bought
                 var planRequest = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/marketplace_listing/plans/{planId}/accounts?per_page=100");
                 planRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
                 planRequest.Headers.Add("User-Agent", "ImgBot");
@@ -57,6 +61,7 @@ namespace MarketplaceSyncFunction
                 var planResponse = await HttpClient.SendAsync(planRequest);
                 var planJson = await planResponse.Content.ReadAsStringAsync();
 
+                Console.WriteLine(planJson);
                 Account[] accountsInPlan = new Account[0];
                 try
                 {
@@ -69,6 +74,9 @@ namespace MarketplaceSyncFunction
 
                 logger.LogInformation("MarketplaceSync found {NumPlans} for {PlanId}", accountsInPlan.Length, planId);
 
+                // loop through the plans and get all purchases from github in order to sync with our database
+                // ie. sync all purchases that are on git and not saved on our side
+                Dictionary<int, bool> gitData = new Dictionary<int, bool>();
                 foreach (var account in accountsInPlan)
                 {
                     var row = new Common.TableModels.Marketplace(account.id, account.login)
@@ -77,10 +85,43 @@ namespace MarketplaceSyncFunction
                         PlanId = account.marketplace_purchase.plan.id
                     };
                     await marketplaceTable.ExecuteAsync(TableOperation.InsertOrMerge(row));
-                }
-            }
 
-            logger.LogInformation("MarketplaceSync finished");
+                    // create a map to easily check the plans at the next step
+                    gitData.Add(account.id, true);
+                }
+
+                // get all purchases for a plan from our database
+                // then get all purchases for the plan from the github api
+                // remove anything that is in our database and not on the github api response
+                var query = new TableQuery<Common.TableModels.Marketplace>().Where(
+                    "PlanId eq " + planId.ToString()).Take(1000);
+
+                TableContinuationToken contToken = null;
+                var queryOptions = new TableRequestOptions();
+
+                do
+                {
+                    var rows = await marketplaceTable.ExecuteQuerySegmentedAsync(query, contToken);
+                    contToken = rows.ContinuationToken;
+                    if (!rows.Any())
+                        continue;
+
+                    bool redundantVariable;
+                    foreach (var purchase in rows)
+                    {
+                        if (gitData.TryGetValue(purchase.AccountId, out redundantVariable))
+                        {
+                            continue;
+                        }
+
+                        Console.WriteLine("should be deleted");
+                        Console.WriteLine(purchase.AccountId);
+                    }
+                }
+                while (contToken != null);
+
+                logger.LogInformation("MarketplaceSync finished");
+            }
         }
     }
 }
